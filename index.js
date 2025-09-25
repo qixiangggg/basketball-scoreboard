d3cae29a2916002f5df3562549d7bb5d6a238f04
DesmondChong <91426701+qixiangggg@users.noreply.github.com>
2025-09-25 20:02:33 +0800

    Commit initial files

---
+++ index.js
@@ -0,0 +1,34 @@
+let homeScore = document.getElementById("home-score")
+let guestScore = document.getElementById("guest-score")
+let homeScoreCounter = 0
+let guestScoreCounter = 0
+
+function addOneHomeScore(){
+    homeScoreCounter += 1
+    homeScore.textContent = homeScoreCounter
+}
+
+function addTwoHomeScore(){
+    homeScoreCounter += 2
+    homeScore.textContent = homeScoreCounter
+}
+
+function addThreeHomeScore(){
+    homeScoreCounter += 3
+    homeScore.textContent = homeScoreCounter
+}
+
+function addOneGuestScore(){
+    guestScoreCounter += 1
+    guestScore.textContent = guestScoreCounter
+}
+
+function addTwoGuestScore(){
+    guestScoreCounter += 2
+    guestScore.textContent = guestScoreCounter
+}
+
+function addThreeGuestScore(){
+    guestScoreCounter += 3
+    guestScore.textContent = guestScoreCounter
+}
